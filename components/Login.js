26066a783189c8fed70374154ef19a9e4b6f18df
Isaac B. Conteh <67395857+Zacflixx@users.noreply.github.com>
2021-02-17 07:42:58 +0800

    2nd commit

--- components/Login.js
+++ components/Login.js
@@ -1,14 +1,122 @@
+import {useContext, useState} from 'react';
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import SocialButton from '../components/SocialButton';
 
 export class Login extends Component {
-    render() {
-        return (
-            <View>
-                <Text> This is our Login screen </Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image source={require('../assets/zee.png')} style={styles.logo} />
+        <Text style={styles.text}>Login Test App</Text>
+
+        <FormInput
+          //   labelValue={email}
+          //   onChangeText={(userEmail) => setEmail(userEmail)}
+          placeholderText="Email"
+          iconType="user"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        <FormInput
+          //   labelValue={password}
+          //   onChangeText={(userPassword) => setPassword(userPassword)}
+          placeholderText="Password"
+          iconType="lock"
+          secureTextEntry={true}
+        />
+
+        <FormButton
+          buttonTitle="Sign In"
+          //   onPress={() => login(email, password)}
+        />
+
+        <TouchableOpacity style={styles.forgotButton} onPress={() => {}}>
+          <Text style={styles.navButtonText}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.forgotButton2}
+          //   onPress={() => navigation.navigate('Signup')}
+        >
+          <Text style={styles.navButtonText}>
+            Don't have an acount? Create here
+          </Text>
+        </TouchableOpacity>
+        {Platform.OS === 'android' ? (
+          <View>
+            <SocialButton
+              buttonTitle="Sign In with Facebook"
+              btnType="facebook"
+              color="#4867aa"
+              backgroundColor="#e6eaf4"
+              //   onPress={() => fbLogin()}
+            />
+
+            <SocialButton
+              buttonTitle="Sign In with Google"
+              btnType="google"
+              color="#de4d41"
+              backgroundColor="#f5e7ea"
+              //   onPress={() => googleLogin()}
+            />
+          </View>
+        ) : null}
+      </ScrollView>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    paddingTop: 30,
+  },
+  logo: {
+    // flex: 1,
+    marginTop: 70,
+
+    height: 100,
+    width: 350,
+    // resizeMode: 'cover',
+  },
+  text: {
+    fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 40,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  forgotButton: {
+    marginVertical: 4,
+    alignSelf: 'flex-end',
+  },
+  forgotButton2: {
+    marginVertical: 4,
+    marginBottom: 20,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    fontFamily: 'Lato-Regular',
+  },
+});
 
 export default Login;
